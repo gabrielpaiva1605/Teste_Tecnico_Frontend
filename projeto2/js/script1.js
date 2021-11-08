0f0d7d7dabae5d0ffede7a389e7066e297c0e9c8
gabrielpaiva1605 <84588683+gabrielpaiva1605@users.noreply.github.com>
2021-11-08 17:47:50 -0300

    Add files via upload

---
+++ projeto2/js/script1.js
@@ -0,0 +1,103 @@
+class Produto{
+    constructor(){
+        this.id = 1;
+        this.arrayProdutos = [];
+    }
+
+    salvar(){
+        let produto = this.lerDados();
+
+        if (this.validaCampos(produto)) {
+            this.adicionar(produto);
+        }
+
+        this.listaTabela();
+        this.cancelar();
+    }
+
+    listaTabela() {
+        let tbody =  document.getElementById(tbody);
+        tbody.innerText = '';
+
+        for(let i = 0; i < this.arrayProdutos.length; i++) {
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_produto = tr.insertCell();
+            let td_valor = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_id.innerText = tdocument.getElementById('preco').value;;
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+            td_valor.innerText = this.arrayProdutos[i].preco;
+
+            td_id.classList.add('center');
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'img/botao-editar.png';
+
+            let imgDelete = document.createElement('img');
+            imgDelete.src = 'img/botao-de-deletar.png';
+            imgDelete.setAttribute("onclick", "produto.deletar("+ this.arrayProdutos[i].id +")");
+
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelete);
+
+        }
+    }
+
+    adicionar(produto) {
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+
+    lerDados(){
+        let produto = {}
+
+        produto.id = this.id
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.preco = document.getElementById('preco').value;
+
+        return produto
+    }
+
+    validaCampos(){
+        let msg = '';
+
+        if(produto.nomeProduto == '') {
+            msg += 'Informe o valor do produto \n';
+        }
+
+        if(produto.preco == '') {
+            msg += 'Informe o preço do produto \n';
+        }
+
+        if (msg != '') {
+            alert(msg);
+            return false
+        }
+
+        return true
+    }
+
+    cancelar(){
+        document.getElementById('produto').value = '';
+        document.getElementById('preco').value = '';
+    }
+
+    deletar(id){
+
+        let tbody =  document.getElementById(tbody);
+
+        for(let i = 0; i < this.arrayProdutos.length; i++) {
+            if(this.arrayProdutos[i].id == id) {
+                this.arrayProdutos.splice(i, 1);
+                tbody.deleteRow(i);
+            }
+        }
+
+        console.log(this.arrayProdutos);
+    }
+}
+
+var produto = new Produto();
